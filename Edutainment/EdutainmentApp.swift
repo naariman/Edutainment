4bf18fd8dd261d3583682167d3500e38e0eec579
naariman <pticafugu@gmail.com>
2024-08-09 16:29:54 +0500

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Nariman Nogaibayev on 09.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
